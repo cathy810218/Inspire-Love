89a1bd5cfd0c5fa8814e475bf5e2a6cdf7cd1c5e
Cathy Oun <cathy810218@gmail.com>
2015-08-04 01:54:02 -0700

    quotes added

--- Inspire Love/ViewController.swift
+++ Inspire Love/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+  
+  var quotesToDisplay: [String] = []
+  var count = 0
+  @IBOutlet weak var quoteLabel: UILabel!
+  
+  func jsonData(jsonData : NSData) -> [String] {
+    var error : NSError?
+    if let quote = NSJSONSerialization.JSONObjectWithData(jsonData, options: nil, error: &error) as? [String] {
+      return quote
+    }
+    return []
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    //        self.view.backgroundColor = UIColor(patternImage: UIImage(named: ))
+    if let filepath = NSBundle.mainBundle().pathForResource("TestingArray", ofType: "json") {
+      if let data = NSData(contentsOfFile: filepath) {
+        quotesToDisplay = jsonData(data)
+        quoteLabel.text = quotesToDisplay[count] // initially count = 0
+      }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    var tapGestrue = UITapGestureRecognizer(target: self, action: Selector("handleTapGesture:"))
+    view.addGestureRecognizer(tapGestrue)
+    
+  }
+  
+  func handleTapGesture(tapGesture: UITapGestureRecognizer ) {
+    quoteLabel.text = quotesToDisplay[++count % quotesToDisplay.count] // in order to loop through the whole array over and over again
+  }
+  
+  
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
 }
 
